40495da30ec7d480db17889ab58983f35e3747a2
Spencer Greeff <spencer4908@live.com>
2025-10-07 16:10:35 -0600

    Fixed an issue with accessible text showing in browser.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import { Component, signal } from '@angular/core';
 import { ReferralDirectory } from "./pages/referral-directory/referral-directory";
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-physician-referral',
   imports: [ReferralDirectory],
   template: '<app-referral-directory></app-referral-directory>'
 })
